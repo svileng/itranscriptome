ef795e894d2ad993a4a24e2695f1d1b026798cd6
Svilen Gospodinov <svilen.gospodinov@gmail.com>
2012-02-18 01:56:36 +0000

    Experiments' GSMs - parsing, storage & retrieval.

--- FinalYearProject/SqlFactory.cs
+++ FinalYearProject/SqlFactory.cs
@@ -25,31 +25,55 @@ namespace ExperimentsManager.Helpers
             result.Append("dataset_platform_organism, dataset_platform_technology_type, ");
             result.Append("dataset_feature_count, dataset_sample_organism, dataset_sample_type, ");
             result.Append("dataset_reference_series, dataset_update_date) VALUES (");
-            result.Append("'"); result.Append(e.Database); result.Append("',");
-            result.Append("'"); result.Append(e.DatabaseName); result.Append("',");
-            result.Append("'"); result.Append(e.DatabaseWebLink); result.Append("',");
-            result.Append("'"); result.Append(e.DatabaseInstitute); result.Append("',");
-            result.Append("'"); result.Append(e.DatabaseEmail); result.Append("',");
-            result.Append("'"); result.Append(e.DatabaseRef); result.Append("',");
-            result.Append("'"); result.Append(e.Dataset); result.Append("',");
-            result.Append("'"); result.Append(e.DatasetTitle); result.Append("',");
-            result.Append("'"); result.Append(e.DatasetDescription); result.Append("',");
-            result.Append("'"); result.Append(e.DatasetType); result.Append("',");
-            result.Append("'"); result.Append(e.DatasetPubmedId); result.Append("',");
-            result.Append("'"); result.Append(e.DatasetPlatform); result.Append("',");
-            result.Append("'"); result.Append(e.DatasetPlatformOrganism); result.Append("',");
-            result.Append("'"); result.Append(e.DatasetPlatformTechnologyType); result.Append("',");
-            result.Append("'"); result.Append(e.DatasetFeatureCount); result.Append("',");
-            result.Append("'"); result.Append(e.DatasetSampleOrganism); result.Append("',");
-            result.Append("'"); result.Append(e.DatasetSampleType); result.Append("',");
-            result.Append("'"); result.Append(e.DatasetReferenceSeries); result.Append("',");
-            result.Append("'"); result.Append(e.DatasetUpdateDate); result.Append("')");
+            result.Append("'"); result.Append(e.Database); result.Append("', ");
+            result.Append("'"); result.Append(e.DatabaseName); result.Append("', ");
+            result.Append("'"); result.Append(e.DatabaseWebLink); result.Append("', ");
+            result.Append("'"); result.Append(e.DatabaseInstitute); result.Append("', ");
+            result.Append("'"); result.Append(e.DatabaseEmail); result.Append("', ");
+            result.Append("'"); result.Append(e.DatabaseRef); result.Append("', ");
+            result.Append("'"); result.Append(e.Dataset); result.Append("', ");
+            result.Append("'"); result.Append(e.DatasetTitle); result.Append("', ");
+            result.Append("'"); result.Append(e.DatasetDescription); result.Append("', ");
+            result.Append("'"); result.Append(e.DatasetType); result.Append("', ");
+            result.Append("'"); result.Append(e.DatasetPubmedId); result.Append("', ");
+            result.Append("'"); result.Append(e.DatasetPlatform); result.Append("', ");
+            result.Append("'"); result.Append(e.DatasetPlatformOrganism); result.Append("', ");
+            result.Append("'"); result.Append(e.DatasetPlatformTechnologyType); result.Append("', ");
+            result.Append("'"); result.Append(e.DatasetFeatureCount); result.Append("', ");
+            result.Append("'"); result.Append(e.DatasetSampleOrganism); result.Append("', ");
+            result.Append("'"); result.Append(e.DatasetSampleType); result.Append("', ");
+            result.Append("'"); result.Append(e.DatasetReferenceSeries); result.Append("', ");
+            result.Append("'"); result.Append(e.DatasetUpdateDate); result.Append("'); ");
+
+            result.Append(CreateInsertGSMQueryFromObject(e));
+
+            return result.ToString();
+        }
+
+        public static string CreateInsertGSMQueryFromObject(Experiment e)
+        {
+            if (e == null) {
+                throw new NullReferenceException();
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (ExperimentGSM gsm in e.GSMs) {
+                result.Append("INSERT INTO gsms_data (experiment_id, gsm_id, gsm_value) VALUES (");
+                result.Append("'"); result.Append(e.Dataset); result.Append("', ");
+                result.Append("'"); result.Append(gsm.GSMId); result.Append("', ");
+                result.Append("'"); result.Append(gsm.Value); result.Append("'); ");
+            }
 
             return result.ToString();
         }
 
         public static string CreateSelectExperimentQuery(string condition)
         {
+            if (string.IsNullOrEmpty(condition)) {
+                throw new ArgumentException();
+            }
+
             StringBuilder result = new StringBuilder();
 
             result.Append("SELECT * FROM experiments WHERE ");
@@ -68,30 +92,49 @@ namespace ExperimentsManager.Helpers
 
         internal static string CreateUpdateExperimentQueryFromObject(Experiment e)
         {
+            if (e == null) {
+                throw new NullReferenceException();
+            }
+
             StringBuilder result = new StringBuilder();
 
             result.Append("UPDATE experiments SET ");
-            result.Append("database_used = "); result.Append("'"); result.Append(e.Database); result.Append("',");
-            result.Append("database_name = "); result.Append("'"); result.Append(e.DatabaseName); result.Append("',");
-            result.Append("database_web_link = "); result.Append("'"); result.Append(e.DatabaseWebLink); result.Append("',");
-            result.Append("database_institute = "); result.Append("'"); result.Append(e.DatabaseInstitute); result.Append("',");
-            result.Append("database_email = "); result.Append("'"); result.Append(e.DatabaseEmail); result.Append("',");
-            result.Append("database_ref = "); result.Append("'"); result.Append(e.DatabaseRef); result.Append("',");
-            result.Append("dataset = "); result.Append("'"); result.Append(e.Dataset); result.Append("',");
-            result.Append("dataset_title = "); result.Append("'"); result.Append(e.DatasetTitle); result.Append("',");
-            result.Append("dataset_description = "); result.Append("'"); result.Append(e.DatasetDescription); result.Append("',");
-            result.Append("dataset_type = "); result.Append("'"); result.Append(e.DatasetType); result.Append("',");
-            result.Append("dataset_pubmed_id = "); result.Append("'"); result.Append(e.DatasetPubmedId); result.Append("',");
-            result.Append("dataset_platform = "); result.Append("'"); result.Append(e.DatasetPlatform); result.Append("',");
-            result.Append("dataset_platform_organism = "); result.Append("'"); result.Append(e.DatasetPlatformOrganism); result.Append("',");
-            result.Append("dataset_platform_technology_type = "); result.Append("'"); result.Append(e.DatasetPlatformTechnologyType); result.Append("',");
-            result.Append("dataset_feature_count = "); result.Append("'"); result.Append(e.DatasetFeatureCount); result.Append("',");
-            result.Append("dataset_sample_organism = "); result.Append("'"); result.Append(e.DatasetSampleOrganism); result.Append("',");
-            result.Append("dataset_sample_type = "); result.Append("'"); result.Append(e.DatasetSampleType); result.Append("',");
-            result.Append("dataset_reference_series = "); result.Append("'"); result.Append(e.DatasetReferenceSeries); result.Append("',");
-            result.Append("dataset_update_date = "); result.Append("'"); result.Append(e.DatasetUpdateDate); result.Append("',");
+            result.Append("database_used = "); result.Append("'"); result.Append(e.Database); result.Append("', ");
+            result.Append("database_name = "); result.Append("'"); result.Append(e.DatabaseName); result.Append("', ");
+            result.Append("database_web_link = "); result.Append("'"); result.Append(e.DatabaseWebLink); result.Append("', ");
+            result.Append("database_institute = "); result.Append("'"); result.Append(e.DatabaseInstitute); result.Append("', ");
+            result.Append("database_email = "); result.Append("'"); result.Append(e.DatabaseEmail); result.Append("', ");
+            result.Append("database_ref = "); result.Append("'"); result.Append(e.DatabaseRef); result.Append("', ");
+            result.Append("dataset = "); result.Append("'"); result.Append(e.Dataset); result.Append("', ");
+            result.Append("dataset_title = "); result.Append("'"); result.Append(e.DatasetTitle); result.Append("', ");
+            result.Append("dataset_description = "); result.Append("'"); result.Append(e.DatasetDescription); result.Append("', ");
+            result.Append("dataset_type = "); result.Append("'"); result.Append(e.DatasetType); result.Append("', ");
+            result.Append("dataset_pubmed_id = "); result.Append("'"); result.Append(e.DatasetPubmedId); result.Append("', ");
+            result.Append("dataset_platform = "); result.Append("'"); result.Append(e.DatasetPlatform); result.Append("', ");
+            result.Append("dataset_platform_organism = "); result.Append("'"); result.Append(e.DatasetPlatformOrganism); result.Append("', ");
+            result.Append("dataset_platform_technology_type = "); result.Append("'"); result.Append(e.DatasetPlatformTechnologyType); result.Append("', ");
+            result.Append("dataset_feature_count = "); result.Append("'"); result.Append(e.DatasetFeatureCount); result.Append("', ");
+            result.Append("dataset_sample_organism = "); result.Append("'"); result.Append(e.DatasetSampleOrganism); result.Append("', ");
+            result.Append("dataset_sample_type = "); result.Append("'"); result.Append(e.DatasetSampleType); result.Append("', ");
+            result.Append("dataset_reference_series = "); result.Append("'"); result.Append(e.DatasetReferenceSeries); result.Append("', ");
+            result.Append("dataset_update_date = "); result.Append("'"); result.Append(e.DatasetUpdateDate); result.Append("', ");
             result.Append("tags = "); result.Append("'"); result.Append(e.Tags); result.Append("' ");
             result.Append("WHERE id = "); result.Append(e.Id);
+
+            return result.ToString();
+        }
+
+        public static string CreateSelectGSMsForExperimentQuery(string experiment_id)
+        {
+            if (string.IsNullOrEmpty(experiment_id)) {
+                throw new ArgumentException();
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            result.Append("SELECT * from gsms_data WHERE experiment_id = '");
+            result.Append(experiment_id);
+            result.Append("'");
 
             return result.ToString();
         }
